dd3398c6ad44f14e10f92d399cb8de5d43d8cc68
Aidan Kennedy <72538907+kennedyaidan@users.noreply.github.com>
2021-11-29 14:12:24 -0800

    Create Generate_Tim1i.m

---
+++ Generate_Tim1i.m
@@ -0,0 +1,8 @@
+function [T] = Generate_Tim1i(alpha,a,d,theta)
+
+T = [cos(theta)                 -sin(theta)             0              a;
+     sin(theta)*cos(alpha)   cos(theta)*cos(alpha)   -sin(alpha)   -sin(alpha)*d;
+     sin(theta)*sin(alpha)   cos(theta)*sin(alpha)    cos(alpha)    cos(alpha)*d;
+     0                          0                        0             1        ];
+
+end
